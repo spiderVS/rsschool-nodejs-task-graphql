fbb9ee1493e0e18480dbeb05a5587693a8483056
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-26 20:42:35 +0300

    feat: add handlers for all posts routes -> posts test => ok

--- src/routes/posts/index.ts
+++ src/routes/posts/index.ts
@@ -1,53 +1,90 @@
-// import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-// import { idParamSchema } from '../../utils/reusedSchemas';
-// import { createPostBodySchema, changePostBodySchema } from './schema';
-// import type { PostEntity } from '../../utils/DB/entities/DBPosts';
+import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
+import { idParamSchema } from '../../utils/reusedSchemas';
+import { createPostBodySchema, changePostBodySchema } from './schema';
+import type { PostEntity } from '../../utils/DB/entities/DBPosts';
 
-// const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
-//   fastify
-// ): Promise<void> => {
-//   fastify.get('/', async function (request, reply): Promise<PostEntity[]> {});
+const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> => {
+  fastify.get('/', async function (request, reply): Promise<PostEntity[]> {
+    const posts = await fastify.db.posts.findMany();
+    return posts;
+  });
 
-//   fastify.get(
-//     '/:id',
-//     {
-//       schema: {
-//         params: idParamSchema,
-//       },
-//     },
-//     async function (request, reply): Promise<PostEntity> {}
-//   );
+  fastify.get(
+    '/:id',
+    {
+      schema: {
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      const { id } = request.params;
+      const post = await fastify.db.posts.findOne({ key: 'id', equals: id });
+      if (post) {
+        return post;
+      } else {
+        throw fastify.httpErrors.notFound();
+      }
+    }
+  );
 
-//   fastify.post(
-//     '/',
-//     {
-//       schema: {
-//         body: createPostBodySchema,
-//       },
-//     },
-//     async function (request, reply): Promise<PostEntity> {}
-//   );
+  fastify.post(
+    '/',
+    {
+      schema: {
+        body: createPostBodySchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      const { body } = request;
+      const { userId } = body;
+      const isUserExist = !!(await fastify.db.users.findOne({ key: 'id', equals: userId }));
+      if (isUserExist) {
+        const createdPost = await fastify.db.posts.create(body);
+        return createdPost;
+      } else {
+        throw fastify.httpErrors.badRequest();
+      }
+    }
+  );
 
-//   fastify.delete(
-//     '/:id',
-//     {
-//       schema: {
-//         params: idParamSchema,
-//       },
-//     },
-//     async function (request, reply): Promise<PostEntity> {}
-//   );
+  fastify.delete(
+    '/:id',
+    {
+      schema: {
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      const { id } = request.params;
+      try {
+        const deletedPost = await fastify.db.posts.delete(id);
+        return deletedPost;
+      } catch {
+        throw fastify.httpErrors.badRequest();
+      }
+    }
+  );
 
-//   fastify.patch(
-//     '/:id',
-//     {
-//       schema: {
-//         body: changePostBodySchema,
-//         params: idParamSchema,
-//       },
-//     },
-//     async function (request, reply): Promise<PostEntity> {}
-//   );
-// };
+  fastify.patch(
+    '/:id',
+    {
+      schema: {
+        body: changePostBodySchema,
+        params: idParamSchema,
+      },
+    },
+    async function (request, reply): Promise<PostEntity> {
+      const { id } = request.params;
+      const { body } = request;
+      const user = await fastify.db.posts.findOne({ key: 'id', equals: id });
+      if (user) {
+        const changedPost = await fastify.db.posts.change(id, body);
+        return changedPost;
+      } else {
+        throw fastify.httpErrors.badRequest();
+      }
+    }
+  );
+};
 
-// export default plugin;
+export default plugin;
