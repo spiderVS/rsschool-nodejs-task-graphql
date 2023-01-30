f72e63ca29cde4192731795695fa88efdc01dea7
Давлюд Анатолий Георгиевич <davlud_ag@bytechs.by>
2023-01-30 03:49:40 +0300

    feat: add and comment create fake enities to DB.ts

--- src/utils/DB/DB.ts
+++ src/utils/DB/DB.ts
@@ -4,6 +4,32 @@ import DBProfiles from './entities/DBProfiles';
 import DBUsers from './entities/DBUsers';
 import * as lodash from 'lodash';
 
+// import { faker } from '@faker-js/faker';
+
+// export const generate_createUserDTO = () => ({
+//   firstName: faker.name.firstName(),
+//   lastName: faker.name.lastName(),
+//   email: faker.internet.email(),
+// });
+
+// export const generate_createProfileDTO = (userId: string, memberTypeId: string) => ({
+//   userId,
+//   memberTypeId,
+//   avatar: faker.image.avatar(),
+//   sex: faker.name.sexType(),
+//   birthday: faker.date.birthdate().getTime(),
+//   country: faker.address.country(),
+//   street: faker.address.street(),
+//   city: faker.address.city(),
+// });
+
+// export const generate_createPostDTO = (userId: string) => ({
+//   userId,
+//   title: faker.lorem.sentence(),
+//   content: faker.lorem.sentences(5),
+// });
+
+
 export default class DB {
   users = new DBUsers();
   profiles = new DBProfiles();
@@ -11,12 +37,8 @@ export default class DB {
   posts = new DBPosts();
 
   constructor() {
-  // const user = this.users.create({
-  //   firstName: 'Vasyan',
-  //   lastName: 'Pupkin',
-  //   email: 'vasyan@gmail.com',
-  // });
-  // console.log('🚀 user:', user);
+
+    // this.createFakeEntities();
 
     const deepCopyResultTrap: ProxyHandler<any> = {
       get: (target, prop) => {
@@ -37,4 +59,19 @@ export default class DB {
       this[k as keyof typeof this] = new Proxy(v, deepCopyResultTrap);
     }
   }
+
+  // createFakeEntities = async () => {
+  //   for (let i = 0; i < 2; i++) {
+  //     const user = await this.users.create(
+  //       generate_createUserDTO()
+  //       );
+  //       console.log('🚀 user:', user);
+
+  //     const post = await this.posts.create(generate_createPostDTO(user.id));
+  //     console.log('🚀 post:', post);
+
+  //     const profile = await this.profiles.create(generate_createProfileDTO(user.id, Math.random() < 0.5 ? 'basic' : 'business'));
+  //     console.log('🚀 profile:', profile);
+  //   }
+  // };
 }
